402a25394bb5d345c28c4e77fa457a775a0423c9
Patricia Tami Sakita <tamisakita@hotmail.com>
2024-06-19 21:00:33 -0700

    add useEffect to Button component

--- components/CustomButtonHome.js
+++ components/CustomButtonHome.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Dimensions } from "react-native";
 import { Button } from "@rneui/themed";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
-// getting the screen dimensions
-const { width } = Dimensions.get("window");
-
-// calculating the width based on screen size/ gap and padding
-const buttonWidth = width / 2 - 30;
+// Function to calculate button width based on screen dimensions
+const calculateButtonWidth = () => {
+  const { width } = Dimensions.get("window");
+  return width / 2 - 30; // Adjust the calculation as needed
+};
 
 export default function CustomButtonHome({ title, icon }) {
+  const [buttonWidth, setButtonWidth] = useState(calculateButtonWidth);
+
+  // Use useEffect to handle screen resizing
+  useEffect(() => {
+    const updateButtonWidth = () => {
+      setButtonWidth(calculateButtonWidth());
+    };
+
+    // Add event listener for screen dimension changes
+    Dimensions.addEventListener("change", updateButtonWidth);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      Dimensions.removeEventListener("change", updateButtonWidth);
+    };
+  }, []);
+
   return (
     // for this custom button I've used the Button component from @rneui/themed with its respective props
     <Button
-      buttonStyle={styles.customButtonContainer}
+      buttonStyle={{ width: buttonWidth, height: buttonWidth }}
       title={title}
       icon={<FontAwesomeIcon icon={icon} size={55} color="white" />}
       iconPosition="top"
     ></Button>
   );
 }
-
-// Custom style for the Button component
-const styles = StyleSheet.create({
-  customButtonContainer: {
-    width: buttonWidth,
-    height: buttonWidth,
-  },
-});
